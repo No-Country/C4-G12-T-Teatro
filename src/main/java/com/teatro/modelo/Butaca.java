844ce1d3c2eab7861554c28996221601af527e48
Martin <iriartemartin2@gmail.com>
2022-03-30 16:13:56 -0300

    Se agrega modelo butaca

---
+++ src/main/java/com/teatro/modelo/Butaca.java
@@ -0,0 +1,22 @@
+package com.teatro.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Butaca {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+}
