b615d5329716758a44f0edfbfb960f1383d6346e
Martin <iriartemartin2@gmail.com>
2022-04-03 22:29:14 -0300

    Se crean los modelos de promocion y promocionPorcentual.

--- src/main/java/com/teatro/modelo/PromocionPorcentual.java
+++ src/main/java/com/teatro/modelo/PromocionPorcentual.java
@@ -1,27 +1,35 @@
 package com.teatro.modelo;
 
+import java.util.List;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("Porcentual")
 @NoArgsConstructor
-@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class PromocionPorcentual extends Promocion {
-	
+
 	private float porcentajeACobrar;
-	
+
+	public PromocionPorcentual(String titulo, String urlImagen, List<Show> shows, float descuento) {
+		super(null, titulo, urlImagen, true, shows);
+		this.porcentajeACobrar = (float) ((100 - descuento) / 100.0);
+	}
+
 	@Override
 	public float getPrecio() {
 		return super.getPrecio() * porcentajeACobrar;
 	}
-	
+
 	@Override
 	public boolean esNula() {
 		return false;
 	}
-
 }
