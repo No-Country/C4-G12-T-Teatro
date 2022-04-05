d2c8beca0f1c4f1b11d8ca7ed75f3401a55cf93d
Martin <iriartemartin2@gmail.com>
2022-04-05 15:50:33 -0300

    algo

--- src/main/java/com/teatro/modelo/Sala.java
+++ src/main/java/com/teatro/modelo/Sala.java
@@ -1,9 +1,15 @@
 package com.teatro.modelo;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,4 +24,16 @@ public class Sala {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Column(unique = true)
+	private String nombre;
+	
+	@Min(0)
+	private int capacidad;
+	
+	private boolean activa = true;
+	
+	@OneToMany(mappedBy = "sala")
+	private List<Butaca> butacas;
 }
--- src/main/java/com/teatro/modelo/Seccion.java
+++ src/main/java/com/teatro/modelo/Seccion.java
@@ -1,11 +1,11 @@
 package com.teatro.modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,25 +13,16 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class Butaca {
-	
+@AllArgsConstructor
+public abstract class Seccion {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private boolean activa = true;
+	@NotBlank
+	@Column(unique = true)
+	private String nombre;
 	
-	@ManyToOne
-	@JoinColumn(name = "sala_id")
-	private Sala sala;
-	
-	/*@ManyToOne
-	@JoinColumn(name = "ticket_id")
-	private Ticket ticket;*/
-	
-	@ManyToOne
-	@JoinColumn(name = "seccion_id")
-	private Seccion seccion;
 }
