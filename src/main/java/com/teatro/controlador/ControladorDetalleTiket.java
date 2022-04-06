ff8e97d3c7740dbf732db6177bdba2b986e1c117
Marcos <69874820+marcosep192000@users.noreply.github.com>
2022-04-06 15:27:36 -0300

    controlador Tiket

--- src/main/java/com/teatro/controlador/ControladorDetalleTiket.java
+++ src/main/java/com/teatro/controlador/ControladorDetalleTiket.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,8 +27,12 @@ public class ControladorDetalleTiket {
 		 return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
+	@GetMapping ("/detalle/listar")
+	@ResponseBody
 	public List<DetalleTiket> listadoDetalle(){
-		return null;
+          
+	   return detalleServicio.ListarDetalle(); 
+		
 	
 	}
 	
