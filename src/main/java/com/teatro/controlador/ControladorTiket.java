cd62cae3c5598655e32a648e7f2809b58df95d8c
Marcos <69874820+marcosep192000@users.noreply.github.com>
2022-04-19 17:26:34 -0300

    crud incompleto tiket

--- src/main/java/com/teatro/controlador/ControladorTiket.java
+++ src/main/java/com/teatro/controlador/ControladorTiket.java
@@ -1,9 +1,13 @@
 package com.teatro.controlador;
 import java.util.List;
+import java.util.Optional;
 
-import jdk.javadoc.internal.doclets.toolkit.builders.BuilderFactory;
+
+import com.teatro.modelo.Usuario;
+import com.teatro.servicio.UsuarioServicio;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -18,35 +22,60 @@ import javax.validation.Valid;
 @RestController
 public class ControladorTiket {
 
-
+    private  final UsuarioServicio usuarioServicio;
     private final TiketServicio tiketService;
 
-    
-
     @PostMapping("/tiket/new")
-    public ResponseEntity<?>  crear(@RequestBody Tiket tiketNuevo, BindingResult bindingResult){
+    public ResponseEntity<?>  crear(@Valid @RequestBody Tiket tiketNuevo, BindingResult bindingResult){
     	if (bindingResult.hasErrors())
-            return new  ResponseEntity("campos mal ingresados",HttpStatus.BAD_REQUEST);
+            return new  ResponseEntity<>( bindingResult.getAllErrors(),HttpStatus.BAD_REQUEST);
         tiketService.guardar(tiketNuevo);
-    	    return  new ResponseEntity<>(HttpStatus.CREATED) ;
+    	    return  new ResponseEntity<>("Tiket  Creado",HttpStatus.CREATED) ;
+
+
     }
+
+    // listar todos TIKET
+
     @GetMapping("/tiket")
     @ResponseBody
     public List<Tiket>tiket(){
         return tiketService.buscarTodos();
-
     }
 
+    //ELIMINAR TIKET
     @DeleteMapping("/tiket/eliminar/{id}")
-    public ResponseEntity eliminar(@Valid @RequestParam Long id, BindingResult bindingResult){
-
+    public ResponseEntity eliminar(@Valid @PathVariable("id") Long id, BindingResult bindingResult){
         if (bindingResult.hasErrors())
-            return new ResponseEntity(  "no se encontro el tiket a eliminar",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(  "no se encontro el tiket Eliminar",HttpStatus.BAD_REQUEST);
         tiketService.borrarPorId(id);
-        return null;
+        return new ResponseEntity( "Tiket eliminado",HttpStatus.NO_CONTENT );
+        // ex
     }
 
+    //bUSCAR POR ID
+    @GetMapping("/tiket/{id}")
+    public ResponseEntity<Tiket> buscarPorId(@PathVariable("id") Long id){
+       if (tiketService.existePorId(id))
+           return new ResponseEntity ( tiketService.buscarPorId(id), HttpStatus.OK);
+       return new ResponseEntity(  "No se encontro el tiket",HttpStatus.BAD_REQUEST);
 
-	
-	
+        }
+
+ // BUSCAR POR USUARIO
+
+        @GetMapping("/tiket/comprador/{comprador}")
+        @ResponseBody
+        public List<Tiket> buscarPorComprador(@PathVariable("comprador") Long comprador){
+        if (usuarioServicio.existePorId(comprador)
+
+           return tiketService.buscarPorComprador(comprador);
+        }
+
+     //   @GetMapping("/tiket/show")
+
+        //
+
+
+
 }
--- src/main/java/com/teatro/servicio/TiketServicio.java
+++ src/main/java/com/teatro/servicio/TiketServicio.java
@@ -1,11 +1,15 @@
 package com.teatro.servicio;
 
+import com.teatro.modelo.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.teatro.modelo.Tiket;
 import com.teatro.repositorio.TiketRepositorio;
 import com.teatro.servicio.base.BaseServicio;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TiketServicio extends BaseServicio<Tiket, Long, TiketRepositorio> {
@@ -15,5 +19,10 @@ public class TiketServicio extends BaseServicio<Tiket, Long, TiketRepositorio> {
 	@Autowired
 	public TiketServicio(TiketRepositorio repositorio) {
 		super(repositorio);
+	}
+
+
+	public List<Tiket> buscarPorComprador(Long comprador) {
+      return  repositorio.findByComprado(comprador);
 	}
 }
